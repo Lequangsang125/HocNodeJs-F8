1a72d65475a7f384f8f1f99fd72d9e3f6536a5cc
Le Quang Sang <sanglq125@gmail.com>
2025-02-16 21:01:08 +0700

    .

--- 1.Bắt đầu/5.Install ExpressJS/Blog/src/app/controllers/CourseController.js
+++ 1.Bắt đầu/5.Install ExpressJS/Blog/src/app/controllers/CourseController.js
@@ -1,68 +1,113 @@
-const Course = require("../models/Course")
+const Course = require("../models/Course");
 
 class CourseController {
-    //GET search
+    /**
+     * Hiển thị thông tin khóa học dựa vào slug
+     * @route GET /courses/:slug
+     */
     show(req, res, next) {
         Course.findOne({ slug: req.params.slug }).lean()
             .then((course) => {
-                res.render('courses/show', { course })
+                res.render('courses/show', { course });
             })
-            .catch(next)
+            .catch(next);
     }
 
+    /**
+     * Hiển thị trang tạo khóa học
+     * @route GET /courses/create
+     */
     create(req, res) {
-        res.render('courses/create')
+        res.render('courses/create');
     }
 
-    //cap nhat
+    /**
+     * Hiển thị trang chỉnh sửa khóa học
+     * @route GET /courses/:id/edit
+     */
     edit(req, res, next) {
         Course.findById(req.params.id).lean()
             .then(course => res.render('courses/edit', { course }))
-            .catch(next)
+            .catch(next);
     }
 
+    /**
+     * Lưu khóa học mới vào database
+     * @route POST /courses/store
+     */
     store(req, res, next) {
-        req.body.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`
+        // Tạo ảnh thumbnail từ YouTube video ID
+        req.body.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`;
+
+        // Tạo đối tượng Course và lưu vào DB
         const course = new Course(req.body);
         course.save()
             .then(() => res.redirect('/me/stored/courses'))
-            .catch(next)
+            .catch(next);
     }
 
+    /**
+     * Cập nhật thông tin khóa học
+     * @route PUT /courses/:id
+     */
     update(req, res, next) {
         Course.updateOne({ _id: req.params.id }, req.body)
             .then(() => res.redirect('/me/stored/courses'))
-            .catch(next)
+            .catch(next);
     }
-    // Xóa mềm khóa học
+
+    /**
+     * Xóa mềm khóa học (chuyển vào thùng rác)
+     * @route DELETE /courses/:id
+     */
     delete(req, res, next) {
-        Course.deleteById(req.params.id)  // Sử dụng deleteById
+        Course.deleteById(req.params.id)  // Sử dụng deleteById để xóa mềm
             .then(() => res.redirect('back'))
             .catch(next);
     }
 
+    /**
+     * Xóa vĩnh viễn khóa học khỏi database
+     * @route DELETE /courses/:id/force
+     */
     forceDelete(req, res, next) {
         Course.deleteOne({ _id: req.params.id })
             .then(() => {
-                console.log(`Đã xóa vĩnh viễn khóa học ${req.params.id}`);
+                console.log(`🔴 Đã xóa vĩnh viễn khóa học ${req.params.id}`);
                 res.redirect("back");
             })
             .catch(next);
     }
 
+    /**
+     * Khôi phục khóa học đã bị xóa mềm
+     * @route PATCH /courses/:id/restore
+     */
     restore(req, res, next) {
         Course.restore({ _id: req.params.id })
-            .then(result => {
-                console.log('Restore thành công:', result);
+            .then(() => {
+                console.log(`✅ Restore thành công khóa học ${req.params.id}`);
                 res.redirect('back');
             })
-            .catch(err => {
-                console.error('Lỗi khi restore:', err);
-                next(err);
-            });
+            .catch(next);
     }
 
+    /**
+     * Xử lý hành động hàng loạt (Xóa nhiều khóa học)
+     * @route POST /courses/handle-form-actions
+     */
+    handleFormActions(req, res, next) {
+        switch (req.body.action) {
+            case 'delete':
+                Course.delete({ _id: { $in: req.body.courseIds } })  // Xóa mềm nhiều khóa học
+                    .then(() => res.redirect('back'))
+                    .catch(next);
+                break;
+            default:
+                res.json({ message: '⚠ Hành động không hợp lệ!' });
+        }
+    }
 }
 
-
-module.exports = new CourseController
+// Xuất module `CourseController`
+module.exports = new CourseController();
